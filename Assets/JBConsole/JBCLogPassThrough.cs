880f6abf8d1057df68c033d4f46329a73c094ef9
Lu Aye Oo <lu@spaceapegames.com>
2013-11-24 14:35:54 +0000

    log pass through plugin.

---
+++ Assets/JBConsole/JBCLogPassThrough.cs
@@ -0,0 +1,53 @@
+using UnityEngine;
+
+public class JBCLogPassThrough : MonoBehaviour
+{
+	const string CHANNEL = "Unity";
+
+	void SetupUnityToJBC()
+	{
+		Application.RegisterLogCallback(HandleUnityLog);
+	}
+	
+
+	void HandleUnityLog(string logString, string stackTrace, LogType type)
+	{
+		if(type == LogType.Error || type == LogType.Exception)
+		{
+			var msg = logString + "\n" + type + "\n" + stackTrace;
+			JBLogger.ErrorCh(CHANNEL, msg);
+		}
+		else if (type == LogType.Warning)
+		{
+			JBLogger.WarnCh(CHANNEL, logString);
+		}
+		else
+		{
+			JBLogger.InfoCh(CHANNEL, logString);
+		}
+	}
+
+	public static JBCLogPassThrough RegisterUnityToJBC()
+	{
+		if(JBConsole.Exists)	
+		{
+			var plugin = JBConsole.instance.RegisterPlugin<JBCLogPassThrough>();
+			plugin.SetupUnityToJBC();
+			return plugin;
+		}
+		return null;
+	}
+
+	/*
+	 todo
+	public static JBCLogPassThrough RegisterJBCToUnity()
+	{
+		if(JBConsole.Exists)	
+		{
+			var plugin = JBConsole.instance.RegisterPlugin<JBCLogPassThrough>();
+			plugin.SetupJBCToUnity()
+			return plugin;
+		}
+		return null;
+	}*/
+}
